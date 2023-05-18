fc57e3bb098f3afe8e2ae6f89e428957b75c4fab
user <aleksandrsemyonkin1980@gmail.comgit config --global user.login Alekssemonkin>
2023-05-18 16:32:11 +0300

    layuot basic

--- src/route/index.js
+++ src/route/index.js
@@ -256,7 +256,7 @@ router.get('/work', function (req, res) {
 router.get('/program', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('program', {
-    layout: 'program',
+    layout: 'basic',
     program: {
       excursion: {
         name: 'Cultural Tour',
